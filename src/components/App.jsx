bd9649ec9170c0891ce2480793aed42860f9d256
Justin Kwan <jkwan19@gmail.com>
2020-12-24 22:08:16 -0800

    Implement example navigation menu bar

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import NavigationBar from './NavigationBar'
+
+function App() {
+
+  return (
+    <div>
+      <NavigationBar />
+      <div>Videos</div>
+      <div>Blog</div>
+    </div>
+
+  );
+}
+export default App;
