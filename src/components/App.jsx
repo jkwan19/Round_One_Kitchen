6845554886794d0f4176effb2d8cd34e9353edd8
Justin Kwan <jkwan19@gmail.com>
2020-12-25 15:44:34 -0800

    Compile ingredients JSON file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavigationBar from './NavigationBar';
 import YoutubePlayer from './YoutubePlayer';
 import Recipe from './Recipe';
+import data from '../ingredients.json'
 
 function App() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    setIngredients(data);
+  }, [])
+
   return (
     <div>
       <NavigationBar />
--- src/components/YoutubePlayer.jsx
+++ src/components/YoutubePlayer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 
-function YouTubePlayer() {
+function YoutubePlayer() {
   const opts = {
     height: '390',
     width: '640',
@@ -9,14 +9,14 @@ function YouTubePlayer() {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
-  }
-  const _onReady = (event) => {
+  };
+  const onReady = (event) => {
     // access to player in all event handlers via event.target
     event.target.pauseVideo();
-  }
+  };
   return (
-    <YouTube videoId="32iji_1Nduo" opts={opts} onReady={_onReady} />
+    <YouTube videoId="32iji_1Nduo" opts={opts} onReady={onReady} />
   );
-};
+}
 
-export default YouTubePlayer;
+export default YoutubePlayer;
