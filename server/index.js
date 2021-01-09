3b0e362aea72a623451761aedb800c127c2db70f
Justin Kwan <jkwan19@gmail.com>
2021-01-08 21:58:32 -0800

    Create bare mongo and model for reviews

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
