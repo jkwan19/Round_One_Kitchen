a5c5c56c07d8c835593559e1c3cf1e1318f6c935
Justin Kwan <jkwan19@gmail.com>
2021-01-08 22:55:08 -0800

    Test CRUD operations successful via Postman

--- server/index.js
+++ server/index.js
@@ -5,11 +5,16 @@ const path = require('path');
 const app = express();
 const apiPort = 8080;
 
+const db = require('../database');
+const reviewRouter = require('./reviews-router');
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use('/api', reviewRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
