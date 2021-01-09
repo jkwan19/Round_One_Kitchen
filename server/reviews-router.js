c000fa3123584c27ce0161b100656038b5e5503e
Justin Kwan <jkwan19@gmail.com>
2021-01-08 22:27:07 -0800

    Set up bare CRUDS operations for reviews

---
+++ server/reviews-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const Controller = require('./reviews-controller')
+
+const router = express.Router()
+
+router.post('/reviews', Controller.createReview)
+router.put('/reviews/:id', Controller.updateReview)
+router.delete('/reviews/:id', Controller.deleteReview)
+router.get('/reviews/:id', Controller.getReviewById)
+router.get('/reviews', Controller.getReviews)
+
+module.exports = router
